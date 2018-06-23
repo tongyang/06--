a86e438e24e31a8a90bca61fb62bf818e2898e89
扬 童 <452949529@qq.com>
2018-06-24 02:01:03 +0800

    复习一下冒泡排序

--- 06-函数/Program.cs
+++ 06-函数/Program.cs
@@ -10,6 +10,27 @@ namespace _06_函数
     {
         static void Main(string[] args)
         {
+            #region 先复习一边冒泡排序
+            int[] numbers = { 3, 3, 2, 1, 321, 32, 323, 111, 32, 5, 6, 31 };
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+            #endregion
 
         }
     }

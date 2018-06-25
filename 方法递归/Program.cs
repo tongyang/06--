d6feb32c4698fae2f4a1b5ae74b53c258fcbae88
扬 童 <452949529@qq.com>
2018-06-25 13:12:59 +0800

    递归的一些细节说明

--- 方法递归/Program.cs
+++ 方法递归/Program.cs
@@ -8,9 +8,27 @@ namespace 方法递归
 {
     class Program
     {
+        public static int i = 0;
+
         static void Main(string[] args)
         {
+            TellStory();
+        }
 
+        /// <summary>
+        /// 方法递归可以用：从A点到B点需要经过很多的门，无论是正常通过还是原路返回都必须经过这些门
+        /// </summary>
+        public static void TellStory()
+        {
+            Console.WriteLine("从前有座山");
+            Console.WriteLine("山上有座庙");
+            i++;
+            if (i >= 10)
+            {
+                return;
+                
+            }
+            TellStory();
         }
     }
 }

8e8c7055aae5cbeb3db026de70356616ae47a25c
扬 童 <452949529@qq.com>
2018-06-25 15:35:11 +0800

    完成第四第五个练习

--- 方法综合练习/Program.cs
+++ 方法综合练习/Program.cs
@@ -19,11 +19,115 @@ namespace 方法综合练习
             #endregion
 
             #region 第二个练习
-            string[] strs = {"马龙", "迈克尔乔丹", "雷吉米勒", "蒂姆邓肯", "科比布莱恩特" };
-            Console.WriteLine(ShowMax(strs));
+            //string[] strs = {"马龙", "迈克尔乔丹", "雷吉米勒", "蒂姆邓肯", "科比布莱恩特" };
+            //Console.WriteLine(ShowMax(strs));
+            #endregion
+
+            #region 第三个练习
+            //int[] nums = {1, 3, 4, 55, 2131, 13213, 123213 };
+            //Console.WriteLine("该整数数组的平均值为：{0}", ShowAvg(nums)); 
+            #endregion
+
+            #region 第四个练习
+            //Console.WriteLine("请输入一个数字：");
+            //string s = Console.ReadLine();
+            //int number;
+            //IsNumber(s, out number);
+            //if (IsPrime(number))
+            //{
+            //    Console.WriteLine("您输入的是一个质数");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("您输入的不是一个质数");
+            //}
+            #endregion
+
+            #region 第五个练习
+            //Console.WriteLine("请输入您的分数：");
+            //string s = Console.ReadLine();
+            //int scoreInt;
+            //IsNumber(s, out scoreInt);
+            //double score = Convert.ToDouble(scoreInt);
+            //string level;
+            //ShowLevel(score, out level);
+            //Console.WriteLine("您的成绩为：{0}", level);
             #endregion
         }
 
+        public static void ShowLevel(double score, out string level)
+        {
+            level = "差";
+            if (score >= 90 && score <= 100)
+            {
+                level = "优";
+            }
+            else if (score >= 80 && score < 90)
+            {
+                level = "良";
+            }
+            else if (score >= 60 && score < 70)
+            {
+                level = "中";
+            }
+            else if (score >= 70 && score < 80)
+            {
+                level = "一般";
+            }
+            else
+            {
+                level = "差";
+            }
+            
+        }
+
+        public static void IsNumber(string s, out int number)
+        {
+            number = 0;
+            while (true)
+            {
+                try
+                {
+                    number = Convert.ToInt32(s);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("您输入的不是数字，请重新输入：");
+                    s = Console.ReadLine();
+                    continue;
+                }
+            }
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num > 1)
+            {
+                for (int i = 2; i < 10; i++)
+                {
+                    if (i % num == 0 && i != num)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static int ShowAvg(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum / nums.Length;
+        }
 
         public static string ShowMax(string[] strs)
         {
